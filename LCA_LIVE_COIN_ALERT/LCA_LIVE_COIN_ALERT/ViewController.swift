eaf4b4cf8de9feee17dede9c42bc5f2b39a79b96
Naga Kukunuru <nkukunur@mtu.edu>
2018-03-22 11:58:07 -0400

    Intial Commit

---
+++ LCA_LIVE_COIN_ALERT/LCA_LIVE_COIN_ALERT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LCA_LIVE_COIN_ALERT
+//
+//  Created by Naga Kukunuru on 3/22/18.
+//  Copyright © 2018 HovaFoundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
